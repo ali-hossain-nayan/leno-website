ad97906255b58b27f9e7af29f22bc50ff8786a72
Nayan1234l <ali15-13943@diu.edu.bd>
2025-11-27 15:09:42 +0600

    updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Mobile Menu
     const toggleButton = document.querySelector('.navbar__mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar__mobile-menu-items');
 
@@ -7,27 +6,21 @@ document.addEventListener('DOMContentLoaded', function () {
         mobileMenu.classList.toggle('active');
     });
 
-    // Video Modal
     const modal = document.getElementById('videoModal');
     const videoButton = document.querySelector('.preview__video-button');
     const closeButton = document.querySelector('.modal__close-button');
     const videoPlayer = document.getElementById('videoPlayer');
 
-    // Open modal when clicked
     videoButton.addEventListener('click', function () {
-        // Show modal
         modal.style.display = 'block';
 
-        //Replace the src attribute with the video URL
         videoPlayer.src = 'https://www.youtube.com/embed/8sXRyHI3bLw';
 
-        // Close modal on close button click
         closeButton.addEventListener('click', function () {
             modal.style.display = 'none';
             videoPlayer.src = '';
         });
 
-        // Close modal on outter click
         window.addEventListener('click', function (event) {
             if (event.target == modal) {
                 modal.style.display = 'none';
@@ -37,7 +30,6 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-// Navigation background on scroll
 window.addEventListener('scroll', function () {
     const navbar = document.querySelector('.navbar');
 
